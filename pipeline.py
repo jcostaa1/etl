4a135a6a89966cb5ca226ef992a09acff2ac8491
jcostaa1 <jcsilvaaa203@gmail.com>
2025-02-18 18:45:02 -0300

    fix: adding many comments

--- pipeline.py
+++ pipeline.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from datetime import datetime
 
-def extracting(): #extracting data
+#extracting data
+def extracting(): 
     df = pd.read_csv('raw_data.csv', delimiter=';', quotechar='"')
     df.columns = ['nome', 'score', 'peso', 'data_nascimento', 'nome_da_mãe', 'data_nascimento_mãe', 'telefone', 'cpf', 'endereco', 'email', 'ocupacao', 'renda_mensal', 'status_cadastro', 'data_cadastro']
     return df
 
-def cleansing(data, pattern=r'\D'): #cleansing document type
+#cleansing document type
+def cleansing(data, pattern=r'\D'): 
     return data.str.replace(pattern, '', regex=True).fillna('')
-
 df = extracting()
 df['telefone'] = cleansing(df['telefone'])
 df['cpf'] = cleansing(df['cpf'], r'[^\d]')
 
-def converting(df, cols, types, errors='coerce'): #converting types
+#converting types
+def converting(df, cols, types, errors='coerce'): 
     for col, datatype in types.items():
         if col in cols:
             if datatype == 'numeric':
@@ -23,7 +25,6 @@ def converting(df, cols, types, errors='coerce'): #converting types
             elif datatype == 'bool':
                 df[col] = df[col].astype(bool)
         return df
-    
 df = extracting()
 converted = df.columns
 columns = {
@@ -36,16 +37,17 @@ columns = {
     }
 df = converting(df, converted, columns)
 
-def formatting(df, column_name='endereco'): #adjusting address
+#adjusting address
+def formatting(df, column_name='endereco'): 
     adjust1 = df[column_name].str.split(r',|-', expand=True)
     adjust1.columns = ['rua', 'numero', 'bairro', 'estado']
     df = pd.concat([df, adjust1], axis=1)
     df = df.drop('endereco', axis=1)
     return df
-
 df = formatting(df)
 
-df.to_csv('csv_data.csv') #testing a csv file w/ transformed data
+#testing a csv file w/ transformed data
+df.to_csv('csv_data.csv') 
 
 today = datetime.now() #unnecessary message
 message = f"Arquivo pronto. \nDia e horário: {today.strftime('%d/%m/%Y %H:%M:%S')}."
